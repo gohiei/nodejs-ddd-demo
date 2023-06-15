441d5f4c381bb72e431302efc6fed322c5e9730a
chuck <chuck@mail.chungyo.net>
2023-06-15 10:13:10 +0800

    style: Remove uselsss comment & import

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,8 +6,6 @@ import { UserModule } from './user/user.module';
 import { CreateDatabaseImports } from './infra/database.provider';
 import { CreateRedisImports } from './infra/redis.provider';
 import { LoggerModule } from './logger/logger.module';
-import { RedisModule } from '@liaoliaots/nestjs-redis';
-import { ID_REDIS_NAMESPACE } from './user/user.constant';
 
 @Module({
   imports: [
@@ -16,13 +14,6 @@ import { ID_REDIS_NAMESPACE } from './user/user.constant';
     }),
     ...CreateDatabaseImports(),
     ...CreateRedisImports(),
-    // RedisModule.forRoot({
-    //   config: {
-    //     namespace: ID_REDIS_NAMESPACE,
-    //     host: 'localhost',
-    //     port: 6379,
-    //   },
-    // }),
     UserModule,
     LoggerModule,
   ],
